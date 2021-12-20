bc5a248e9ded3e5406a362571b9f8bee0fa99452
Michael Killingbeck <8310367+michaelkillingbeck@users.noreply.github.com>
2021-12-20 22:49:02 +0000

    Completed Day1 Part2

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -1,20 +1,51 @@
-lines = []
-lastLine = 0
-count = 0
-part1Answer = 0
-sum = 0
-lastSum = 0
+def part_one():
+    count =  0
+    lastLine = 0
+    lines = []
+    part1Answer = 0
 
-with open('Day1Puzzle1Input.txt') as file:
-    lines = file.readlines()
+    with open('Day1Puzzle1Input.txt') as file:
+        lines = file.readlines()
 
-for line in lines:
-    count += 1
-    newLine = int(line)
+    for line in lines:
+        count += 1
+        newLine = int(line)
 
-    if (newLine > lastLine & lastLine != 0):
-        part1Answer += 1
+        if (newLine > lastLine & lastLine != 0):
+            part1Answer += 1
 
-    lastLine = newLine
+        lastLine = newLine
 
-print (part1Answer)
+    print (part1Answer)
+
+def part_two():
+    count = 0
+    currentSum = 0
+    lastSum = 0
+    lines = []
+    part2Answer = 0
+    queue = []
+    
+    with open('Day1Puzzle1Input.txt') as file:
+        lines = file.readlines()
+
+    for line in lines:
+        count += 1
+
+        if(count > 3):
+            queue.pop(0)
+
+        queue.append(int(line))
+
+        if (count >= 3):
+            currentSum = queue[0] + queue[1] + queue[2]
+    
+        if (currentSum > lastSum & lastSum != 0):
+            part2Answer += 1
+
+        lastSum = currentSum
+
+    print(part2Answer)
+
+part_one()
+part_two()
