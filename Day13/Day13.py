31a72bbba4e0e23822d0117874cb8308ba96e320
Michael Killingbeck <8310367+michaelkillingbeck@users.noreply.github.com>
2022-01-09 12:04:04 +0000

    Finished Day13.

--- Day13/Day13.py
+++ Day13/Day13.py
@@ -1,4 +1,4 @@
-input_file = "SmallSample.txt"
+input_file = "Day13PuzzleInput.txt"
 
 class Fold:
     direction = ""
@@ -7,8 +7,8 @@ class Fold:
     def __init__(self, instruction):
         sections = instruction.split('=')
 
-        direction = sections[0]
-        value = sections[1]
+        self.direction = sections[0]
+        self.value = int(sections[1])
 
     def to_string(self):
         return str("Direction %s, Value %s" % (self.direction, self.value))
@@ -26,6 +26,16 @@ class Point:
     def to_string(self):
         return str("%s , %s" % (self.x, self.y))
 
+def count_dots(page):
+    count = 0
+
+    for row in range(len(page)):
+        for column in range(len(page[0])):
+            if(page[row][column] == '#'):
+                count += 1
+
+    return count
+
 def create_dots_array(x, y, dots):
     page = [['.' for column in range(x + 1)] for row in range(y + 1)]
 
@@ -33,6 +43,47 @@ def create_dots_array(x, y, dots):
         page[dot.y][dot.x] = '#'
 
     return page
+
+def fold_left(fold_value, page):
+    folded_page = [['.' for column in range(fold_value)] for row in range(len(page))]
+    
+    for row in range(len(page)):
+        for column in range(len(folded_page[row])):
+            folded_page[row][column] = page[row][column]
+
+    current_column = 0
+
+    while(current_column < fold_value):
+        source_index = (fold_value * 2) - current_column
+
+        for row in range(len(folded_page)):
+            if(source_index < len(page[row])):
+                if(page[row][source_index] == '#'):
+                    folded_page[row][current_column] = '#'
+
+        current_column += 1
+
+    return folded_page
+
+
+def fold_up(fold_value, page):
+    folded_page = [['.' for column in range(len(page[0]))] for row in range(fold_value)]
+   
+    for row in range(len(folded_page)):
+        for column in range(len(folded_page[row])):
+            folded_page[row][column] = page[row][column]
+
+    for index in range(fold_value):
+        source_index = (fold_value * 2) - index
+
+        if(source_index < len(page)):
+            source_row = page[source_index]
+
+            for cell in range(len(source_row)):
+                if(source_row[cell] == '#'):
+                    folded_page[index][cell] = '#'
+
+    return folded_page
 
 def get_dots_layout(filename):
     lines = []
@@ -82,8 +133,27 @@ def get_maximum_y(dots):
 
     return local_max
 
+def perform_folds(folds, page):
+    folded_page = page
+
+    for fold in folds:
+        #print(fold.to_string())
+        if(fold.direction == 'y'):
+            folded_page = fold_up(fold.value, folded_page)
+        elif(fold.direction == 'x'):
+            folded_page = fold_left(fold.value, folded_page)
+
+        #for line in folded_page:
+            #print(line)
+        visible_dots = count_dots(folded_page)
+        print("There are %s visible dots" %visible_dots)
+
+    for line in folded_page:
+        print("".join(line))
+
 dots = get_dots_layout(input_file)
 max_x = get_maximum_x(dots)
 max_y = get_maximum_y(dots)
 dots_array = create_dots_array(max_x, max_y, dots)
 fold_instructions = get_folds(input_file)
+perform_folds(fold_instructions, dots_array)
